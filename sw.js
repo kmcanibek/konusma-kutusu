fd834c23e5ef4418c7729bea026c59561974ffb2
kmcanibek <kmcanibek@gmail.com>
2026-09-21 17:31:49 +0300

    Önbelleği güncel sürüme geçir

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'konusma-kutusu-v1';
+const CACHE = 'konusma-kutusu-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -16,10 +16,14 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(cached => cached || caches.match('./index.html'))
+      )
   );
 });
