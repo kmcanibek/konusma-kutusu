bc6db9de0001245e86aa740272d5eb506e907a10
kmcanibek <kmcanibek@gmail.com>
2026-09-22 09:41:22 +0300

    Yeni yerleşim için önbelleği yenile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'konusma-kutusu-v2';
+const CACHE = 'konusma-kutusu-layout-v3';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
